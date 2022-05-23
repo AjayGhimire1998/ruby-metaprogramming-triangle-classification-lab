c0b461c504882cc22930b58fdc0fa22844732354
AjayGhimire <ajayghimire1998@gmail.com>
2022-05-23 19:09:43 +1000

    commit tree bcfda68c45c7ebd9b748c65637498cbd323b6bba

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize a, b, c
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind 
+    validate_triangle
+    if a == b && b == c 
+      return :equilateral
+    elsif b == c || a == b || a == c
+      return :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def validate_triangle 
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      if side <= 0 
+        real_triangle << false 
+      end
+      raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
